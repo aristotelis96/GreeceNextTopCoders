3301ee597e53444eedee1c782fff06f63d4e197e
IrrelevantX <alexman665@hotmail.com>
2019-01-04 19:18:09 +0200

    add products paraponietai

--- Website/controllers/products.js
+++ Website/controllers/products.js
@@ -33,18 +33,79 @@ module.exports = {
             let categoryInput= req.body.categoryInput;
             let startdateInput= req.body.startdate;
             let enddateInput= req.body.enddate;
-            let checkstartInput = req.body.start;
+            let checkstartInput = req.body.start;    // check if date is given 
             let checkendInput = req.body.end;
 
             if (checkendInput == "on" && checkstartInput == "on"){
-
+                //do nothing ? 
             }else if (checkstartInput == "on"){
-                
+                enddateInput = "NULL";
             }else if (checkendInput == "on"){
-
+                startdateInput = "NULL";            // if no date given, put null
             }else { 
-
+                startdateInput = "NULL";
+                enddateInput = "NULL";
             }
+            //check if product exists from an other store
+            //if not, add it first, else just get the id 
+            //then add price and store to the other table
+            dbProducts.returnProduct(productInput, function ( error, results, field){
+                if (error){
+                    return res.send({
+                        "code": 404,
+                        "failed": "error while accesing db"
+                    });
+                }else {
+                    if (!(results.length > 0)){ //if  not existing in db,insert
+                        dbProducts.InsertInProducts({
+                            name : productInput,
+                            description : decriptionInput,
+                            category : categoryInput
+                        }, (err, ress)=>{
+                            if (err)
+                            return res.send(err);
+                        }); // now it sure is in db, get the id 
+                    }
+                    dbProducts.returnShopID(companyInput, function( shopError,resultShop,shopFields){
+                        if (shopError){
+                            return res.send({
+                                "code": 4004,
+                                "failed": "error while accesing db for shop id"
+                            });
+                        }
+                    else{
+                    dbProducts.returnProductID(productInput, function( errorr,resultProduct,fields){
+                        if (errorr){
+                            return res.send({
+                                "code": 4004,
+                                "failed": "error while accesing db for product id"
+                            });
+                        }else { // now we got the id in result(???), inster in table prices
+                            console.log(resultProduct);
+                            dbProducts.InsertInPrices({
+                                price: priceInput,
+                                dateFrom: startdateInput,
+                                dateTo: enddateInput,
+                                productID: resultProduct,              // edw paraponietai,"a foreign key constraint fails"
+                                shopID: resultShop
+                            },(insertError,InsertResult) =>{
+                                if(insertError)                // pray for no errors....
+                                    return res.send(insertError);
+                                else{
+                                    var sess = req.session; // if all is good, go to home page
+                                    sess.email = emailInput;
+                                    return res.redirect('/');
+                                }
+                            })
+                        }
+                    })
+                }
+                })
+
+
+                }
+            })
+
         }
 
     }
--- Website/models/products.js
+++ Website/models/products.js
@@ -12,5 +12,56 @@ const pool = require('./index').getPool();
             }
         });
     }
-    
-    module.exports = {getAllProducts}
+    returnProduct = function (name, callback) {
+        let query = "SELECT * FROM products WHERE name = " + pool.escape(name);
+        pool.query(query, (err, results) => {
+            if (err){
+                return callback(err);
+            } else {
+                return callback(null, results);
+            }
+        })
+    }
+    InsertInProducts = function (fields, callback){
+        let vals = " VALUES(" + pool.escape(fields.name) + "," + pool.escape(fields.description) + "," + pool.escape(fields.category) + ");" ;
+        let query = "INSERT INTO products (name, description, category)" + vals;
+        pool.query(query, (err,result)=>{
+            if(err) {
+                return callback(err);
+            } else {
+                return callback (null, result);
+            }
+        })
+    }
+    returnProductID = function (name, callback) {
+        let query = "SELECT id FROM products WHERE name = " + pool.escape(name);
+        pool.query(query, (err, results) => {
+            if (err){
+                return callback(err);
+            } else {
+                return callback(null, results);
+            }
+        })
+    }
+    InsertInPrices = function (fields, callback){
+        let vals = " VALUES(" + pool.escape(fields.price) + "," + pool.escape(fields.dateFrom) + "," + pool.escape(fields.dateTo) +"," + pool.escape(fields.productID)+"," + pool.escape(fields.shopID) + ");" ;
+        let query = "INSERT INTO prices (price, dateFrom, dateTo, productID, shopID)" + vals;
+        pool.query(query, (err,result)=>{
+            if(err) {
+                return callback(err);
+            } else {
+                return callback (null, result);
+            }
+        })
+    }
+    returnShopID = function (name, callback) {
+        let query = "SELECT id FROM shops WHERE name = " + pool.escape(name);
+        pool.query(query, (err, results) => {
+            if (err){
+                return callback(err);
+            } else {
+                return callback(null, results);
+            }
+        })
+    }
+    module.exports = {getAllProducts, returnProduct, InsertInProducts, returnProductID, InsertInPrices, returnShopID}
