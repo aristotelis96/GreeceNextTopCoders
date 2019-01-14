98ba6ca9826f483185966f52091e6c1f78180bad
IrrelevantX <alexman665@hotmail.com>
2019-01-14 06:48:15 +0200

    PLEASE READ DESCRIPTION LADS AND LASSES!
    1)sunartish kainouria sto model users gia return me exact id, dn alla3a thn hdh uparxon giati pernei mono mia eisodo pou 8ewrei dedomeno oti einai to emailgia na mn ginoun mperdemata me alles fores pou kaleite se alla merh tou kwdika
    2) gia na mpeite sthn wannabe selida katasthmatos einai to url /user/X, opou x to ID tou katastimatos(valid id 1-7 kai 60-63, pou einai 3 magazia me to idio onoma giati ekana mia blakia)
    3) Ta maps griniazoun giati dn exw energopoihsei to free trial, douleuoun kanonika sthn telikh kanoume to free trial prin thn e3etash 3erwgw
    P.S. O patrick sto PageNotFound hr8e gia na meinei, h menoume mazi h feugoume mazi apo thn omada >:(

--- Website/controllers/shopPage.js
+++ Website/controllers/shopPage.js
@@ -2,33 +2,47 @@ const fs = require('fs');
 const session = require('express-session');
 const dbShops = require(appDir + '/models/shops.js')
 const dbaddresses = require(appDir + '/models/addresses.js')
+const dbusers = require(appDir + '/models/users.js')
 shoppage = function (req, res) {
-    var urlID = req.params;  // this gets the parameter from url
-    console.log(urlID[0])
-    dbShops.returnExactShop( {id: urlID[0]} , (err, result) => {
-        console.log(result)
+    var urlID = req.params;  // this gets the parameter from url   
+    dbShops.returnExactShop({ id: urlID[0] }, (err, result) => {
         if (err)
             return res.status(666).send("Error in accesing Db\n" + err);
         if (result.length == 0)
             return res.redirect('/shopnotfound');
-        dbaddresses.getAddress(result[0].id, (err, resultaddress) =>{
+        dbaddresses.getAddress(result[0].id, (err, resultaddress) => {
             if (err)
-            return res.status(666).send("Error in accesing Db for addresses\n" + err);
-            console.log(result[0].id);
-            console.log(resultaddress[0].city);
-            console.log(resultaddress[0].periferia);
-            res.render("shopPage.ejs",{
-                login: req.session.login,
-                title: "Σελίδα καταστήματος",
-                name: req.session.email,
-                shopname: result[0].name,
-                shopphone: result[0].phone,
-                shoplng: result[0].lng,
-                shoplat:result[0].lat,
-                shopuserID: result[0].userID,
-                shopcity: resultaddress[0].city,
-                shopperiferia: resultaddress[0].periferia             
+                return res.status(666).send("Error in accesing Db for addresses\n" + err);
+            dbusers.returnUserByID(result[0].userID, (err, resultuser) => {
+                if (err)
+                    return res.status(666).send("Error in accesing Db for users\n" + err);
+                let usermail;
+                if (resultuser.length == 0)
+                    usermail = null;
+                else
+                    usermail = resultuser[0].email;
+                let lang, lati;
+                if (result[0].lat == null || result[0].lng == null) {
+                    lang = 99999;
+                    lati = lang;
+                } else {
+                    lang = result[0].lng;
+                    lati = result[0].lat
+                }
+                res.render("shopPage.ejs", {
+                    login: req.session.login,
+                    title: "Σελίδα καταστήματος",
+                    name: req.session.email,
+                    shopname: result[0].name,
+                    shopphone: result[0].phone,
+                    shoplng: lang,
+                    shoplat: lati,
+                    shopuseremail: usermail,
+                    shopcity: resultaddress[0].city,
+                    shopperiferia: resultaddress[0].periferia
+                })
             })
+
         })
     })
 
